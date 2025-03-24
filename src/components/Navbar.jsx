9cad0f990bcea8a16f245d8146009c13f5b89aef
Abhay Agrawal <awesome.abhay20@gmail.com>
2025-03-24 18:59:05 +0530

    asf

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="min-w-screen p-2 bg-violet-800 flex justify-between md:px-50">
+    <nav className="min-w-screen p-5 bg-violet-800 flex justify-between md:px-50">
         <div className="logo font-bold text-xl text-white">iTask</div>
-        <div className='flex h-full gap-10'>
+        <div className='flex h-full gap-5 md:gap-10'>
             <div className='text-white text-lg font-medium cursor-pointer transition-all hover:scale-110'>Home</div>
             <div className='text-white text-lg font-medium cursor-pointer transition-all hover:scale-110'>Your Tasks</div>
         </div>
